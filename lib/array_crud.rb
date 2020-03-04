152c99a41441cb1ccfe54e8c5482702cd576b748
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-04 09:42:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  return []
+  array = []
 end
 
 def create_an_array
-  numbers = [1, 2, 3, 4, 5]
+  numbers = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
